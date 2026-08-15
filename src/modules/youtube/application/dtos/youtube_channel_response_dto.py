f8a0884a9abecb1dc924855bc16143740c917bd4
ericksonlopes <ofc.erickson@gmail.com>
2026-08-15 14:23:35 -0300

    feat: add optional external_id field to YouTubeChannelResponseDTO

--- src/modules/youtube/application/dtos/youtube_channel_response_dto.py
+++ src/modules/youtube/application/dtos/youtube_channel_response_dto.py
@@ -1,11 +1,14 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class YouTubeChannelResponseDTO(BaseModel):
     id: int
+    external_id: Optional[str] = None
     name: str
     url: str
     active: bool
 
     class Config:
         from_attributes = True
+
